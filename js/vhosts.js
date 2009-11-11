9d39d74048bef6c986fc61a4df6e0aaad37e0e55
tarao <tarao.gnn@gmail.com>
2009-11-11 14:44:06 +0000

    More intelligent alias system.

--- js/vhosts.js
+++ js/vhosts.js
@@ -29,25 +29,32 @@
                     1: {
                         hot: 'hotentry',
                         list: 'entrylist',
+                        new: 'entrylist',
                         e: 'entry',
+                        tag: 't',
                     },
                     2: {
                         fav: 'favorite',
-                        soc: 'social',
-                        eco: 'economics',
-                        politics: 'economics',
-                        pol: 'economics',
-                        ent: 'entertainment',
-                        sports: 'entertainment',
-                        star: 'entertainment',
-                        science: 'knowledge',
-                        sci: 'knowledge',
-                        academic: 'knowledge',
-                        ac: 'knowledge',
-                        web: 'it',
-                        anime: 'game',
-                        neta: 'fun',
                     },
+                    hotentry: {
+                        2: {
+                            soc: 'social',
+                            eco: 'economics',
+                            politics: 'economics',
+                            pol: 'economics',
+                            ent: 'entertainment',
+                            sports: 'entertainment',
+                            star: 'entertainment',
+                            science: 'knowledge',
+                            sci: 'knowledge',
+                            academic: 'knowledge',
+                            ac: 'knowledge',
+                            web: 'it',
+                            anime: 'game',
+                            neta: 'fun',
+                        },
+                    },
+                    get entrylist() { return this.hotentry; },
                 },
             },
         },
@@ -55,20 +62,22 @@
             domain: 'status-code.com',
         },
     }, ARGV.defs);
+
     (function(d, def, ar) {
         if (!def) throw TypeError('no definition for ' + ARG0);
         var dom = def.domain;
-        var sub = ar.shift() || def.sub;
-        var resolve = function(alias, i, name) {
-            return (alias[i]||{})[name] || (alias._||{})[name];
-        };
-        var a1 = def.alias||{};
-        sub = resolve(a1, 0, sub) || sub;
-        var a2 = (def.alias||{})[sub]||{};
-        ar = ar.map(function(v,i) {
-            return resolve(a2,i+1,v) || resolve(a1,i+1,v) || v;
-        });
+
+        ar = Array.reduce((ar||[]).length ? ar : [def.sub], function(p, c, i) {
+            var [ path, alias ] = p;
+            var c = (alias[i]||{})[c] || (alias._||{})[c] || c;
+            path.push(c);
+            alias = override(alias, alias[c]||{});
+            return [ path, alias ];
+        }, [ [], def.alias||{} ])[0];
+
+        var sub = ar.shift();
         var dir = ar.join('/');
+
         if (!dir) {
             var re = new RegExp('^.+?://(?:[a-z.]*\\.)?'+dom+'/([^/]+)','i');
             dir = d.location.href.match(re)?RegExp.$1:(def.dirs||[]).join('/');
