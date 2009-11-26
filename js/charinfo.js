07d975dd2d65d37600e30406ce0ca7b39e17bccb
tarao <tarao.gnn@gmail.com>
2009-11-26 04:30:25 +0000

    In case there is no selection.

--- js/charinfo.js
+++ js/charinfo.js
@@ -3,16 +3,14 @@
 //     http://d.hatena.ne.jp/mandel59/20091125/1259157800
 (function(){
     var sel = function() {
-        try {
-            var r = getSelection().getRangeAt(0);
-            var n = r.startContainer;
-            var p = r.startOffset;
-            return (n.nodeType == 3) ?
-                n.nodeValue.substr(p,2) :
-                n.childNodes[p].textContent.substr(0,2);
-        } catch (e) {
-            return;
-        }
+        var s = getSelection();
+        if (!s.toString()) return;
+        var r = s.getRangeAt(0);
+        var n = r.startContainer;
+        var p = r.startOffset;
+        return (n.nodeType == 3) ?
+            n.nodeValue.substr(p,2) :
+            n.childNodes[p].textContent.substr(0,2);
     };
     var v = (ARGV[0] || sel() || '').charCodeAt(0);
     var code = 0xD800 <= v && v <= 0xDB7F ?
